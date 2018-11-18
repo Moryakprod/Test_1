067da7b76b7123a3532a6d358eb0160722198139
Moryak_prod <Moryak.prod@gmail.com>
2018-11-18 11:55:16 +0200

    did everything in class end add exit from press Enter

--- less-6/main2.py
+++ less-6/main2.py
@@ -12,25 +12,36 @@ data['weather'][0]['description']
 
 
 class Weather:
-    def __init__(self, city, temp):
+    def __init__(self, city, description, temp, pressure, humidity, all, speed):
         self.city = city
+        self.description = description
         self.temp = temp
+        self.pressure = pressure
+        self.humidity = humidity
+        self.all = all
+        self.speed = speed
 
     def __str__(self):
         return '''City: %s
-Temp: %s''' % (self.city, self.temp)
+Description: %s
+Temp: %s degree Celsius
+Pressure: %s hPa
+Humidity: %s percent
+Cloudiness: %s percent
+Wind speed: %s m/s''' % (self.city, self.description, self.temp, self.pressure, self.humidity, self.all, self.speed)
 
 
 class WeatherManager:
-    URL = 'https://api.openweathermap.org/data/2.5/weather?q=%s&appid=ba89be5152a3f2584d8fc4de75381070'
+    URL = 'https://api.openweathermap.org/data/2.5/weather?q=%s&units=metric&lang=ru&appid=ba89be5152a3f2584d8fc4de75381070'
 
-    def get_weather(self, city, lang='ru'):
+    def get_weather(self, city):
         resp = requests.get(self.URL % city)
         data = resp.json()
-        return Weather(data['name'], data['main']['temp'])
+        return Weather(data['name'], data['weather'][0]['description'], data['main']['temp'], data['main']['pressure'], data['main']['humidity'], data['clouds']['all'], data['wind']['speed'])
+    
 
 manager = WeatherManager()
-w = manager.get_weather('London', lang='ru')
-print(w.city)
+w = manager.get_weather('Kharkiv')
+print(w)
 
-
+input('Press ENTER to exit')
